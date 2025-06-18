680e4bc8122e6d64ca77083279678a645e0b5b8d
Harsh Kumar Rai <harshrai24685@gamil.com>
2025-06-18 11:09:18 +0530

    add two line

--- Abc.java
+++ Abc.java
@@ -2,5 +2,7 @@ class Abc
 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints messages to the console.");
     }
-}
+} 
